e4f80c55473bc319a6688e1459453baa5cd75b91
Alex Saveau <saveau.alexandre@gmail.com>
2022-03-06 01:32:59 -0800

    Re-write name cache to use arena style allocation instead of an individual allocation per name
    I also got rid of the separate dir cache since memcpy's are cheap.
    
    Instruction counts went up which is a bummer, but cache misses are 20x lower which is amazing:
    
    ```
    --------------------------------------------------------------------------------
    Ir                   Dr                  Dw                  I1mr           D1mr               D1mw            ILmr           DLmr           DLmw           Bc                  Bcm             Bi                 Bim
    --------------------------------------------------------------------------------
    124,428,240 (100.0%) 29,313,294 (100.0%) 15,577,957 (100.0%) 6,243 (100.0%) 1,144,659 (100.0%) 43,991 (100.0%) 4,140 (100.0%) 2,868 (100.0%) 6,362 (100.0%) 22,625,923 (100.0%) 34,179 (100.0%) 2,475,149 (100.0%) 1,079 (100.0%)  PROGRAM TOTALS
    
    131,017,177 (100.0%) 30,331,510 (100.0%) 15,421,085 (100.0%) 6,585 (100.0%) 42,754 (100.0%) 25,053 (100.0%) 4,129 (100.0%) 2,843 (100.0%) 5,087 (100.0%) 22,522,492 (100.0%) 35,782 (100.0%) 2,474,529 (100.0%) 2,191 (100.0%)  PROGRAM TOTALS
    ```
    
    More importantly, we allocated ~45% fewer blocks:
    
    ```
    Total:     539,521 bytes (100%, 2,823.88/Minstr) in 5,655 blocks (100%, 29.6/Minstr), avg size 95.41 bytes, avg lifetime 69,888,761.12 instrs (36.58% of program duration)
    At t-gmax: 158,560 bytes (100%) in 2,197 blocks (100%), avg size 72.17 bytes
    At t-end:  7,304 bytes (100%) in 7 blocks (100%), avg size 1,043.43 bytes
    Reads:     80,758,723 bytes (100%, 422,695.82/Minstr), 149.69/byte
    Writes:    7,086,836 bytes (100%, 37,092.91/Minstr), 13.14/byte
    
    Total:     474,967 bytes (100%, 2,452.65/Minstr) in 3,537 blocks (100%, 18.26/Minstr), avg size 134.29 bytes, avg lifetime 34,462,064.6 instrs (17.8% of program duration)
    At t-gmax: 115,556 bytes (100%) in 687 blocks (100%), avg size 168.2 bytes
    At t-end:  7,304 bytes (100%) in 7 blocks (100%), avg size 1,043.43 bytes
    Reads:     64,788,183 bytes (100%, 334,555.5/Minstr), 136.41/byte
    Writes:    7,040,592 bytes (100%, 36,356.46/Minstr), 14.82/byte
    ```
    
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/utils/name_cache.rs
+++ src/utils/name_cache.rs
@@ -1,15 +1,40 @@
-use std::mem::ManuallyDrop;
+use std::{
+    cmp::min,
+    mem::{ManuallyDrop, MaybeUninit},
+    ptr, slice,
+};
 
 use tracing::{event, instrument, span, Level};
 
 /// Specialized cache for file names that takes advantage of our monotonically increasing integer
 /// naming scheme.
 ///
-/// We intentionally use a thread-*un*safe raw fixed-size buffer to eliminate an Arc.
+/// A raw fixed-size buffer is intentionally used to eliminate an Arc.
 #[derive(Copy, Clone)]
 pub struct FileNameCache {
-    file_cache: (*mut String, usize),
-    dir_cache: (*mut String, usize),
+    /// The cache can be thought of as an arena, meaning all strings are written into a single
+    /// buffer that only needs to be allocated once. The following memory layout is used:
+    ///
+    /// ```not_rust
+    /// [                               len                                ]
+    /// [            Indices and lengths           |        Strings        ]
+    /// [ [ 2-value packed representation ], ... ] | [ Packed string bytes ]
+    /// [ [ index, index, length, length  ], ... ] | [ Packed string bytes ]
+    /// [ [  u16 ,  u16 ,   u8  ,   u8    ], ... ] | [ Packed string bytes ]
+    /// ```
+    ///
+    /// The goal of this representation is to minimize wasted space storing the string's location.
+    /// Thus, a u16 is used which allows us to store a maximum of 65536 text bytes. Consequentially,
+    /// the maximum number of entries that can be stored is found by this equation:
+    /// `1 + sum_1^5{n * (min(x , 10^n) - 10^(n - 1))} = 2^16`. Solving for `x` yields 15329
+    /// entries. To keep the array 2-byte aligned, entries must come in pairs of two since a single
+    /// entry only uses 3 bytes, so the true maximum is 15328 entries. Finally, the maximum possible
+    /// overall allocation is `15328 * 3 + 65530 = 111520` bytes which seems reasonable as far as
+    /// cache sizes go.
+    buffer: *const u8,
+    len: u32,
+    num_entries: u16,
+    data_start: u16,
 }
 
 unsafe impl Send for FileNameCache {}
@@ -17,85 +42,183 @@ unsafe impl Send for FileNameCache {}
 impl FileNameCache {
     #[instrument(level = "trace")]
     pub fn alloc(files_per_dir: f64, dirs_per_dir: f64) -> Self {
-        let num_cache_entries = files_per_dir + dirs_per_dir;
-        let files_percentage = files_per_dir / num_cache_entries;
-
         // Overestimate since the cache can't grow
-        let num_cache_entries = 1.5 * num_cache_entries;
-        // Max out the cache size at 1MiB
-        let num_cache_entries = f64::min((1 << 20) as f64, num_cache_entries);
+        let num_cache_entries = 1.5 * f64::max(files_per_dir, dirs_per_dir);
+        // Round up to nearst mod 2 to guarantee 2-byte alignment
+        let num_cache_entries = (num_cache_entries.round() as usize + 1) & !1;
+        // Max out the cache size at 15328
+        let num_cache_entries = min(15328, num_cache_entries);
 
-        let file_entries = files_percentage * num_cache_entries;
-        let dir_entries = num_cache_entries - file_entries;
-
-        let alloc_span = span!(Level::TRACE, "file_cache_alloc");
-        let span_guard = alloc_span.enter();
-        let mut file_cache =
-            ManuallyDrop::new(Vec::<String>::with_capacity(file_entries.round() as usize));
-        for (i, entry) in file_cache.spare_capacity_mut().iter_mut().enumerate() {
-            entry.write(FileNameCache::file_name(i));
+        let text_bytes_start = num_cache_entries * 3;
+        let mut text_bytes = 0;
+        {
+            let mut num_cache_entries = num_cache_entries as u32;
+            for n in (1..=5).rev() {
+                // `& !1` to account for x^0 being 1 instead of 0
+                let count = num_cache_entries - min(num_cache_entries, 10u32.pow(n - 1) & !1);
+                text_bytes += n * count;
+                num_cache_entries -= count;
+            }
         }
-        drop(span_guard);
+        let mut buf = ManuallyDrop::new(Vec::with_capacity(
+            num_cache_entries * 3 + text_bytes as usize,
+        ));
 
-        let alloc_span = span!(Level::TRACE, "dir_cache_alloc");
+        let alloc_span = span!(Level::TRACE, "name_gen");
         let span_guard = alloc_span.enter();
-        let mut dir_cache =
-            ManuallyDrop::new(Vec::<String>::with_capacity(dir_entries.round() as usize));
-        for (i, entry) in dir_cache.spare_capacity_mut().iter_mut().enumerate() {
-            entry.write(FileNameCache::dir_name(i));
+
+        let raw_buf: *mut u8 = buf.as_mut_ptr();
+        let mut text_index = 0;
+        for i in 0..num_cache_entries {
+            Self::raw_with_file_name(i, |s| {
+                unsafe {
+                    let (index_index, length_index) = Self::index_to_metadata(i);
+                    (raw_buf.add(index_index) as *mut u16).write(text_index as u16);
+                    raw_buf.add(length_index).write(s.len() as u8);
+
+                    ptr::copy_nonoverlapping(
+                        s.as_ptr(),
+                        raw_buf.add(text_index + text_bytes_start) as *mut u8,
+                        s.len(),
+                    );
+                }
+
+                text_index += s.len();
+            });
         }
-        drop(span_guard);
 
         unsafe {
-            let cap = file_cache.capacity();
-            file_cache.set_len(cap);
-            let cap = dir_cache.capacity();
-            dir_cache.set_len(cap);
+            let cap = buf.capacity();
+            buf.set_len(cap);
         }
 
-        event!(
-            Level::DEBUG,
-            files = file_cache.len(),
-            dirs = dir_cache.len(),
-            "Name cache allocations"
-        );
+        drop(span_guard);
+        event!(Level::DEBUG, size = buf.len(), "Name cache allocated");
 
         Self {
-            file_cache: (file_cache.as_mut_ptr(), file_cache.len()),
-            dir_cache: (dir_cache.as_mut_ptr(), dir_cache.len()),
+            buffer: buf.as_ptr(),
+            len: buf.len() as u32,
+            num_entries: num_cache_entries as u16,
+            data_start: text_bytes_start as u16,
         }
     }
 
     pub fn free(self) {
         unsafe {
-            Vec::from_raw_parts(self.file_cache.0, self.file_cache.1, self.file_cache.1);
-            Vec::from_raw_parts(self.dir_cache.0, self.dir_cache.1, self.dir_cache.1);
+            Vec::from_raw_parts(self.buffer as *mut u8, self.len as usize, self.len as usize);
         }
     }
 
     pub fn with_file_name<T>(self, i: usize, f: impl FnOnce(&str) -> T) -> T {
-        if i >= self.file_cache.1 {
-            return f(&FileNameCache::file_name(i));
+        if i < self.num_entries as usize {
+            let (index_index, length_index) = Self::index_to_metadata(i);
+            unsafe {
+                let index = (self.buffer.add(index_index) as *const u16).read();
+                f(std::str::from_utf8_unchecked(slice::from_raw_parts(
+                    self.buffer.add(index as usize + self.data_start as usize),
+                    self.buffer.add(length_index).read() as usize,
+                )))
+            }
+        } else {
+            Self::raw_with_file_name(i, f)
         }
-
-        f(unsafe { self.file_cache.0.add(i).as_ref().unwrap_unchecked() })
     }
 
     pub fn with_dir_name<T>(self, i: usize, f: impl FnOnce(&str) -> T) -> T {
-        if i >= self.dir_cache.1 {
-            return f(&FileNameCache::dir_name(i));
+        self.with_file_name(i, |s| {
+            let mut buf = [MaybeUninit::<u8>::uninit(); 9];
+            unsafe {
+                let buf_ptr = buf.as_mut_ptr() as *mut u8;
+                ptr::copy_nonoverlapping(s.as_ptr(), buf_ptr, s.len());
+                ptr::copy_nonoverlapping(".dir".as_ptr(), buf_ptr.add(s.len()), 4);
+
+                f(std::str::from_utf8_unchecked(slice::from_raw_parts(
+                    buf.as_ptr() as *const u8,
+                    s.len() + 4,
+                )))
+            }
+        })
+    }
+
+    #[inline]
+    fn index_to_metadata(i: usize) -> (usize, usize) {
+        let metadata_group = i * 3;
+        (
+            metadata_group & !1,
+            metadata_group + 4 - (metadata_group & 1) * 2,
+        )
+    }
+
+    fn raw_with_file_name<T>(i: usize, f: impl FnOnce(&str) -> T) -> T {
+        f(itoa::Buffer::new().format(i))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Deref;
+
+    use super::*;
+
+    struct SafeCache(FileNameCache);
+
+    impl Deref for SafeCache {
+        type Target = FileNameCache;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
         }
-
-        f(unsafe { self.dir_cache.0.add(i).as_ref().unwrap_unchecked() })
     }
 
-    #[inline]
-    fn file_name(i: usize) -> String {
-        i.to_string()
+    impl Drop for SafeCache {
+        fn drop(&mut self) {
+            self.0.free();
+        }
     }
 
-    #[inline]
-    fn dir_name(i: usize) -> String {
-        format!("{}.dir", i)
+    #[test]
+    fn zero_files_allocates_nothing() {
+        let cache = SafeCache(FileNameCache::alloc(0., 0.));
+
+        assert_eq!(cache.len, 0);
+        assert_eq!(cache.num_entries, 0);
+    }
+
+    #[test]
+    fn one_file_allocates_minimal_bytes() {
+        let cache = SafeCache(FileNameCache::alloc(0.9, 0.));
+
+        assert_eq!(cache.len, 8);
+        assert_eq!(cache.num_entries, 2);
+    }
+
+    #[test]
+    fn fifty_files_allocates_minimal_bytes() {
+        let cache = SafeCache(FileNameCache::alloc(50., 0.));
+
+        assert_eq!(cache.len, 370);
+        assert_eq!(cache.num_entries, 76);
+    }
+
+    #[test]
+    fn max_files_allocates_max_bytes() {
+        let cache = SafeCache(FileNameCache::alloc(1e10, 0.));
+
+        assert_eq!(cache.len, 111514);
+        assert_eq!(cache.num_entries, 15328);
+    }
+
+    #[test]
+    fn cached_names_are_returned() {
+        let cache = SafeCache(FileNameCache::alloc(1e10, 0.));
+
+        for i in 0..cache.num_entries as usize * 2 {
+            cache.with_file_name(i, |s| {
+                assert_eq!(s, i.to_string());
+            });
+            cache.with_dir_name(i, |s| {
+                assert_eq!(s, format!("{}.dir", i));
+            });
+        }
     }
 }
