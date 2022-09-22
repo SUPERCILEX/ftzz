51d15c60bb50eb529d745842eb0967da06d8ead0
Alex Saveau <saveau.alexandre@gmail.com>
2022-09-22 05:41:33 -0400

    Make API tests automatically regenerate rustdoc json
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- tests/api.rs
+++ tests/api.rs
@@ -2,15 +2,16 @@ use std::{fs::read_to_string, io::Write};
 
 use goldenfile::Mint;
 use public_api::public_api_from_rustdoc_json_str;
+use rustdoc_json::BuildOptions;
 
 #[test]
 fn api() {
-    println!("Run before test:\n$ cargo +nightly rustdoc --all-features -- -Zunstable-options --output-format json");
+    let json_path = rustdoc_json::build(BuildOptions::default().all_features(true)).unwrap();
 
     let mut mint = Mint::new(".");
     let mut goldenfile = mint.new_goldenfile("api.txt").unwrap();
 
-    let json = read_to_string("target/doc/ftzz.json").unwrap();
+    let json = read_to_string(json_path).unwrap();
     let api = public_api_from_rustdoc_json_str(&json, public_api::Options::default()).unwrap();
     for public_item in api.items {
         writeln!(goldenfile, "{}", public_item).unwrap();
