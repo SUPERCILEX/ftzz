956a169b0dbe18db26cc5f65310abe25b5099a59
Alex Saveau <saveau.alexandre@gmail.com>
2022-10-03 20:47:06 -0700

    Upgrade deps
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- tests/api.rs
+++ tests/api.rs
@@ -1,18 +1,19 @@
 use std::{fs::read_to_string, io::Write};
 
 use goldenfile::Mint;
-use public_api::public_api_from_rustdoc_json_str;
+use public_api::PublicApi;
 use rustdoc_json::BuildOptions;
 
 #[test]
 fn api() {
-    let json_path = rustdoc_json::build(BuildOptions::default().all_features(true)).unwrap();
+    let json_path =
+        rustdoc_json::Builder::build(BuildOptions::default().all_features(true)).unwrap();
 
     let mut mint = Mint::new(".");
     let mut goldenfile = mint.new_goldenfile("api.txt").unwrap();
 
     let json = read_to_string(json_path).unwrap();
-    let api = public_api_from_rustdoc_json_str(&json, public_api::Options::default()).unwrap();
+    let api = PublicApi::from_rustdoc_json_str(&json, public_api::Options::default()).unwrap();
     for public_item in api.items {
         writeln!(goldenfile, "{}", public_item).unwrap();
     }
