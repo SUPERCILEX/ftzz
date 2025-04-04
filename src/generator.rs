c43beb046c8b9b618b1f104eedcc91d1d01aee73
Alex Saveau <saveau.alexandre@gmail.com>
2025-04-03 22:29:23 -0400

    Use lockness
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/generator.rs
+++ src/generator.rs
@@ -9,10 +9,9 @@ use std::{
     fmt::Write,
     fs::create_dir_all,
     hash::{DefaultHasher, Hash, Hasher},
-    num::{NonZeroU64, NonZeroUsize},
+    num::NonZeroU64,
     path::PathBuf,
     process::ExitCode,
-    thread,
 };
 
 use bon::Builder;
@@ -36,8 +35,6 @@ pub enum Error {
     Io,
     #[error("Failed to achieve valid generator environment.")]
     InvalidEnvironment,
-    #[error("Failed to create the async runtime.")]
-    RuntimeCreation,
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
@@ -66,7 +63,7 @@ impl NumFilesWithRatio {
     pub fn new(
         num_files: NonZeroU64,
         file_to_dir_ratio: NonZeroU64,
-    ) -> std::result::Result<Self, NumFilesWithRatioError> {
+    ) -> Result<Self, NumFilesWithRatioError> {
         if file_to_dir_ratio > num_files {
             return Err(NumFilesWithRatioError::InvalidRatio {
                 num_files,
@@ -142,6 +139,7 @@ impl Generator {
     pub fn generate(self, output: &mut impl Write) -> Result<(), Report<Error>> {
         let options = validated_options(self)?;
         print_configuration_info(&options, output)?;
+        log!(Level::Info, "Starting config: {options:?}");
         print_stats(run_generator(options)?, output);
         Ok(())
     }
@@ -353,30 +351,7 @@ fn print_stats(GeneratorStats { files, dirs, bytes }: GeneratorStats, output: &m
 }
 
 #[cfg_attr(feature = "tracing", tracing::instrument(level = "trace"))]
-fn run_generator(config: Configuration) -> Result<GeneratorStats, Report<Error>> {
-    let parallelism = thread::available_parallelism().unwrap_or(NonZeroUsize::new(1).unwrap());
-    let mut runtime = tokio::runtime::Builder::new_current_thread();
-    #[cfg(all(not(miri), target_os = "linux"))]
-    runtime.on_thread_start(|| {
-        use rustix::thread::{UnshareFlags, unshare_unsafe};
-
-        let result = unsafe { unshare_unsafe(UnshareFlags::FILES) };
-        #[cfg(debug_assertions)]
-        result.unwrap();
-        let _ = result;
-    });
-    let runtime = runtime
-        .max_blocking_threads(parallelism.get())
-        .build()
-        .change_context(Error::RuntimeCreation)
-        .attach_opaque(ExitCode::from(sysexits::ExitCode::OsErr))?;
-
-    log!(Level::Info, "Starting config: {config:?}");
-    runtime.block_on(run_generator_async(config, parallelism))
-}
-
-#[cfg_attr(feature = "tracing", tracing::instrument(level = "trace"))]
-async fn run_generator_async(
+fn run_generator(
     Configuration {
         root_dir,
         files,
@@ -390,7 +365,6 @@ async fn run_generator_async(
         seed,
         human_info: _,
     }: Configuration,
-    parallelism: NonZeroUsize,
 ) -> Result<GeneratorStats, Report<Error>> {
     macro_rules! run {
         ($generator:expr) => {{
@@ -399,10 +373,8 @@ async fn run_generator_async(
                 files,
                 dirs_per_dir,
                 max_depth.try_into().unwrap_or(usize::MAX),
-                parallelism,
                 $generator,
             )
-            .await
         }};
     }
 
